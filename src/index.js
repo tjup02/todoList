5e520e4f715995b09cff6837c62fbad71d83c8fe
Amiee <tjup02@gmail.com>
2025-08-19 15:44:40 +0800

    安裝webpack

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./css/reset.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./css/style.scss";
+
+const loginForm = document.querySelector("#loginForm");
+const registerForm = document.querySelector("#registerForm");
+const loginRegisterStyle = document.querySelectorAll(".loginRegisterStyle");
+const loginRegisterBtn = document.querySelectorAll(".loginRegisterBtn");
+
+// 註冊登入頁面切換
+loginRegisterBtn.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    loginRegisterStyle.forEach((i) => {
+      i.classList.remove("active");
+    });
+    if (e.target.getAttribute("id") === "loginBtn") {
+      loginForm.classList.add("active");
+    } else {
+      registerForm.classList.add("active");
+    }
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 處理 CSS 檔案
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i, //處理 SCSS SASS 檔案
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
