78d08fabc9a290b2c8f481b3633da13f742d1f99
Amiee <tjup02@gmail.com>
2025-08-21 17:52:13 +0800

    todoList功能的部分切板完成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./css/style.scss";
 
 const loginForm = document.querySelector("#loginForm");
